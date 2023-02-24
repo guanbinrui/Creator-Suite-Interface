8eb33af17f169e74605834c95636d3f118dd4f37
unclebill <billbill290@gmail.com>
2023-02-24 12:25:27 +0800

    chore: react-router usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/index.css'
 
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home/index.jsx'
 import { Error } from './pages/Error/index.jsx'
 import { Dashboard } from './pages/Dashboard/index.jsx'
@@ -9,15 +9,13 @@ import { WagmiProvider } from './contexts/Wagmi'
 function App() {
     return (
         <WagmiProvider>
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
-                    <Route path="/">
-                        <Route index element={<Home />} />
-                        <Route path="creation/*" element={<Dashboard />} />
-                    </Route>
+                    <Route index element={<Home />}></Route>
+                    <Route path="/creation/*" element={<Dashboard />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </WagmiProvider>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 export function Home() {
     return (
         <>
             <h1>Core Service</h1>
             <ul>
                 <li>
-                    <a href="#/creation">&gt; Creator Suite</a>
+                    <Link to="/creation">&gt; Creator Suite</Link>
                 </li>
             </ul>
         </>
