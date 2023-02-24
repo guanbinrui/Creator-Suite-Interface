cd45b61e144a530d952d8dfa13d7afc4985b44a2
guanbinrui <guanbinrui@dimension.im>
2023-02-24 23:24:23 +0800

    feat: file type

--- src/components/Creations/Creation.jsx
+++ src/components/Creations/Creation.jsx
@@ -1,34 +1,36 @@
 import { formatDistanceToNowStrict } from 'date-fns'
 import { Link } from 'react-router-dom'
 import { formatBalance } from '../../helpers/formatBalance'
+import { getFileType } from '../../helpers/getFileType'
 
 export function Creation(props) {
     return (
         <Link to={`/creation/${props.id}`}>
             <div key={props.id} className="group relative">
                 <div className="min-h-40 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40">
-                    <img
-                        src={props.imageSrc}
-                        alt={props.imageAlt}
-                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
+                    <span className="h-full w-full text-6xl object-cover object-center lg:h-full lg:w-full inline-flex items-center justify-center">
+                        {getFileType(props.attachments[0]).toUpperCase()}
+                    </span>
                 </div>
-                <div className="mt-4 flex justify-between">
-                    <div>
-                        <h3 className="text-sm text-gray-700 font-bold">
-                            <span aria-hidden="true" className="absolute inset-0" />
-                            {props.name}
-                        </h3>
+                <div className="mt-4">
+                    <h3 className="text-sm text-gray-700 font-bold">
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {props.name}
+                    </h3>
+
+                    <div className="flex justify-between ">
                         <p className="mt-1 text-sm text-gray-500">
                             {formatDistanceToNowStrict(props.updatedAt, {
                                 addSuffix: true,
                             })}
                         </p>
+                        <p className="text-sm text-gray-900">
+                            <span className="mr-1 text-gray-500">Price:</span>
+                            <span className="font-medium">
+                                {formatBalance(props.paymentTokenAmount * 10, 1, 2)} DAI
+                            </span>
+                        </p>
                     </div>
-                    <p className="text-sm text-gray-900">
-                        <span className="mr-1 text-gray-500">Price:</span>
-                        <span className="font-medium">{formatBalance(props.paymentTokenAmount, 0, 2)} DAI</span>
-                    </p>
                 </div>
             </div>
         </Link>
