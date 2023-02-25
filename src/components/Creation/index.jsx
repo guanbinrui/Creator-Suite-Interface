e5594f3cf07bcb8b628668019f29d26bd29b546a
guanbinrui <guanbinrui@dimension.im>
2023-02-25 20:21:35 +0800

    refactor: purchase page

--- src/components/Creation/index.jsx
+++ src/components/Creation/index.jsx
@@ -146,19 +146,8 @@ export function Creation() {
                 <div className="mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     {/* Product */}
                     <div className="lg:grid lg:grid-cols-7 lg:grid-rows-1 lg:gap-x-8 lg:gap-y-10 xl:gap-x-16">
-                        {/* Product image */}
-                        <div className="lg:col-span-4 lg:row-end-1">
-                            <div className="aspect-w-4 aspect-h-3 overflow-hidden rounded-lg bg-gray-100">
-                                <img
-                                    src={creation.imageSrc}
-                                    alt={creation.imageAlt}
-                                    className="object-cover object-center"
-                                />
-                            </div>
-                        </div>
-
                         {/* Product details */}
-                        <div className="mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-3 lg:row-span-2 lg:row-end-2 lg:mt-0 lg:max-w-none">
+                        <div className="mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-7 lg:row-span-2 lg:row-end-2 lg:mt-0 lg:max-w-none">
                             <div className="flex flex-col-reverse">
                                 <div className="mt-4">
                                     <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
@@ -186,6 +175,10 @@ export function Creation() {
                                     <div>
                                         <p className="text-green-500">You have bought this creation.</p>
                                     </div>
+                                ) : data.buyerAddresses.length > 0 ? (
+                                    <div>
+                                        <p className="text-gray-500">{`${data.buyerAddresses.length} people purchased it.`}</p>
+                                    </div>
                                 ) : null}
                             </div>
 
@@ -197,7 +190,7 @@ export function Creation() {
                                 {owned || bought ? (
                                     <button
                                         type="button"
-                                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-50 py-3 px-8 text-base font-medium text-blue-700 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                                        className="flex w-1/2 max-w-sm items-center justify-center rounded-md border border-transparent bg-blue-50 py-3 px-8 text-base font-medium text-blue-700 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
                                         onClick={() => setOpenPreviewer(true)}
                                     >
                                         Preview
@@ -205,7 +198,7 @@ export function Creation() {
                                 ) : (
                                     <button
                                         type="button"
-                                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 py-3 px-8 text-base font-medium text-white hover:bg-blue-700 disabled:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                                        className="flex w-1/2 max-w-sm items-center justify-center rounded-md border border-transparent bg-blue-600 py-3 px-8 text-base font-medium text-white hover:bg-blue-700 disabled:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
                                         disabled={isMutating}
                                         onClick={async () => {
                                             if (!isConnected) await connect()
@@ -233,17 +226,6 @@ export function Creation() {
                                               )} DAI for full-access`}
                                     </button>
                                 )}
-                            </div>
-
-                            <div className="mt-10 border-t border-gray-200 pt-10">
-                                <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-                                <div className="prose prose-sm mt-4 text-gray-500">
-                                    <ul role="list">
-                                        {creation.highlights.map((highlight) => (
-                                            <li key={highlight}>{highlight}</li>
-                                        ))}
-                                    </ul>
-                                </div>
                             </div>
 
                             <div className="mt-10 border-t border-gray-200 pt-10">
@@ -317,124 +299,6 @@ export function Creation() {
                                     </li>
                                 </ul>
                             </div>
-                        </div>
-
-                        <div className="mx-auto mt-16 w-full max-w-2xl lg:col-span-4 lg:mt-0 lg:max-w-none">
-                            <Tab.Group as="div">
-                                <div className="border-b border-gray-200">
-                                    <Tab.List className="-mb-px flex space-x-8">
-                                        <Tab
-                                            className={({ selected }) =>
-                                                classNames(
-                                                    selected
-                                                        ? 'border-blue-600 text-blue-600'
-                                                        : 'border-transparent text-gray-700 hover:text-gray-800 hover:border-gray-300',
-                                                    'whitespace-nowrap border-b-2 py-6 text-sm font-medium',
-                                                )
-                                            }
-                                        >
-                                            Customer Reviews
-                                        </Tab>
-                                        <Tab
-                                            className={({ selected }) =>
-                                                classNames(
-                                                    selected
-                                                        ? 'border-blue-600 text-blue-600'
-                                                        : 'border-transparent text-gray-700 hover:text-gray-800 hover:border-gray-300',
-                                                    'whitespace-nowrap border-b-2 py-6 text-sm font-medium',
-                                                )
-                                            }
-                                        >
-                                            FAQ
-                                        </Tab>
-                                        <Tab
-                                            className={({ selected }) =>
-                                                classNames(
-                                                    selected
-                                                        ? 'border-blue-600 text-blue-600'
-                                                        : 'border-transparent text-gray-700 hover:text-gray-800 hover:border-gray-300',
-                                                    'whitespace-nowrap border-b-2 py-6 text-sm font-medium',
-                                                )
-                                            }
-                                        >
-                                            License
-                                        </Tab>
-                                    </Tab.List>
-                                </div>
-                                <Tab.Panels as={Fragment}>
-                                    <Tab.Panel className="-mb-10">
-                                        <h3 className="sr-only">Customer Reviews</h3>
-
-                                        {reviews.featured.map((review, reviewIdx) => (
-                                            <div key={review.id} className="flex space-x-4 text-sm text-gray-500">
-                                                <div className="flex-none py-10">
-                                                    <img
-                                                        src={review.avatarSrc}
-                                                        alt=""
-                                                        className="h-10 w-10 rounded-full bg-gray-100"
-                                                    />
-                                                </div>
-                                                <div
-                                                    className={classNames(
-                                                        reviewIdx === 0 ? '' : 'border-t border-gray-200',
-                                                        'py-10',
-                                                    )}
-                                                >
-                                                    <h3 className="font-medium text-gray-900">{review.author}</h3>
-                                                    <p>
-                                                        <time dateTime={review.datetime}>{review.date}</time>
-                                                    </p>
-
-                                                    <div className="mt-4 flex items-center">
-                                                        {[0, 1, 2, 3, 4].map((rating) => (
-                                                            <StarIcon
-                                                                key={rating}
-                                                                className={classNames(
-                                                                    review.rating > rating
-                                                                        ? 'text-yellow-400'
-                                                                        : 'text-gray-300',
-                                                                    'h-5 w-5 flex-shrink-0',
-                                                                )}
-                                                                aria-hidden="true"
-                                                            />
-                                                        ))}
-                                                    </div>
-                                                    <p className="sr-only">{review.rating} out of 5 stars</p>
-
-                                                    <div
-                                                        className="prose prose-sm mt-4 max-w-none text-gray-500"
-                                                        dangerouslySetInnerHTML={{ __html: review.content }}
-                                                    />
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </Tab.Panel>
-
-                                    <Tab.Panel className="text-sm text-gray-500">
-                                        <h3 className="sr-only">Frequently Asked Questions</h3>
-
-                                        <dl>
-                                            {faqs.map((faq) => (
-                                                <Fragment key={faq.question}>
-                                                    <dt className="mt-10 font-medium text-gray-900">{faq.question}</dt>
-                                                    <dd className="prose prose-sm mt-2 max-w-none text-gray-500">
-                                                        <p>{faq.answer}</p>
-                                                    </dd>
-                                                </Fragment>
-                                            ))}
-                                        </dl>
-                                    </Tab.Panel>
-
-                                    <Tab.Panel className="pt-10">
-                                        <h3 className="sr-only">License</h3>
-
-                                        <div
-                                            className="prose prose-sm max-w-none text-gray-500"
-                                            dangerouslySetInnerHTML={{ __html: license.content }}
-                                        />
-                                    </Tab.Panel>
-                                </Tab.Panels>
-                            </Tab.Group>
                         </div>
                     </div>
                 </div>
