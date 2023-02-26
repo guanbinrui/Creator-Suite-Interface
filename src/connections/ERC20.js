7598317a7ec8f6972c36bab105c5b852417c61e8
guanbinrui <guanbinrui@dimension.im>
2023-02-26 22:23:29 +0800

    feat: UI

---
+++ src/connections/ERC20.js
@@ -0,0 +1,20 @@
+import { readContract } from '@wagmi/core'
+import { polygonMumbai } from '@wagmi/core/chains'
+import ERC20ABI from '../abis/ERC20.json'
+import { isValidAddress } from '../helpers/isValidAddress'
+
+/**
+ * Return the balance of the owner on the ERC20 contract
+ */
+export function balanceOf(address, owner) {
+    if (!isValidAddress(address)) throw new Error('Not a valid contract address.')
+    if (!isValidAddress(owner)) throw new Error('Not a valid owner address.')
+
+    return readContract({
+        chainId: polygonMumbai.chainId,
+        address,
+        abi: ERC20ABI,
+        functionName: 'balanceOf',
+        args: [owner],
+    })
+}
