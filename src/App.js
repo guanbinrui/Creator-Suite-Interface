029940d5c486019ade1f27af3a757d6d8086fb7d
guanbinrui <guanbinrui@dimension.im>
2023-02-27 00:40:29 +0800

    fix: dup css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './styles/index.css'
 
 import { HashRouter, Route, Routes } from 'react-router-dom'
-import { polygonMumbai } from '@wagmi/chains'
 import { Home } from './pages/Home/index.jsx'
 import { Error } from './pages/Error/index.jsx'
 import { Dashboard } from './pages/Dashboard/index.jsx'
@@ -9,7 +8,7 @@ import { WagmiProvider } from './contexts/Wagmi'
 
 function App() {
     return (
-        <WagmiProvider chainId={polygonMumbai.id}>
+        <WagmiProvider>
             <HashRouter>
                 <Routes>
                     <Route path="/">
