20da8927dde5ae8ea5fcb5f78886c4429acc47d1
guanbinrui <guanbinrui@dimension.im>
2023-02-27 10:41:49 +0800

    fix: router

--- src/components/TokenListMenu/index.jsx
+++ src/components/TokenListMenu/index.jsx
@@ -38,7 +38,7 @@ export function TokenListMenu(props) {
                                 <Menu.Item key={x.address} onClick={() => onSelect(x)}>
                                     {({ active }) => (
                                         <a
-                                            href="#/creation/create"
+                                            href="/creation/create"
                                             className={classNames(
                                                 active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                                 'block px-4 py-2 text-sm',
